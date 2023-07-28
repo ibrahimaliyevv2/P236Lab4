0407dd26a3f7be03d1fabcc0ddfe164d82888d17
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2023-07-28 16:31:45 +0400

    task1

---
+++ tasker/Library.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasker
+{
+    internal class Library
+    {
+        public Book[] books;
+        public int count;
+
+        public Library(int capacity)
+        {
+            books = new Book[capacity];
+            count = 0;
+        }
+
+        public void AddBook(string title, string author, int year)
+        {
+            if (count < books.Length)
+            {
+                Book newBook = new Book(title, author, year);
+                books[count] = newBook;
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Kitabxana doludur. Daha cox kitab elave edile bilmez.");
+            }
+        }
+    }
+}
---
+++ tasker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace tasker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library1 = new Library(5);
+
+            library1.AddBook("Danabas kendinin ehvalatlari", "Celil Memmedquluzade", 1960);
+            library1.AddBook("Ferrarisini satan rahib", "Robin Sharm", 2006);
+            library1.AddBook("Yalnizliqdan sizofreniyaya", "OAR", 1999);
+            library1.AddBook("Xeyanet", "Elxan Elatli", 2008);
+            library1.AddBook("Menim sirin portagal agacim", "Joseph Maurino", 2013);
+            library1.AddBook("Antichrist", "Nitsche", 1895);
+
+            Console.WriteLine("Menim lib1 kitablarim:");
+            Console.WriteLine("                                    ");
+
+
+            foreach (Book book in library1.books)
+            {
+                Console.WriteLine($"{book.Title} by {book.Author} in ({book.Year})");
+            }
+
+            Console.WriteLine("                                    ");
+
+            Console.WriteLine("Birinci bu qeder idi.");
+
+            Library library2 = new Library(2);
+
+            Console.WriteLine("                                    ");
+
+            library2.AddBook("Cinayet ve ceza", "Fyodor Dostoyevski", 1970);
+            library2.AddBook("Qurur ve qerez", "Ceyms Ostin", 2023);
+
+            Console.WriteLine("Menim lib2 kitablarim:");
+
+            foreach (Book book in library2.books)
+            {
+                Console.WriteLine($"{book.Title} by {book.Author} in ({book.Year})");
+            }
+
+        }
+    }
+}
